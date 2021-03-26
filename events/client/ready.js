183d261074926415e12e57dd71b8a69dda2a0f2a
Horayo <74557443+Horayo-Glitch@users.noreply.github.com>
2021-03-26 20:50:04 +0100

    Update ready.js

--- events/client/ready.js
+++ events/client/ready.js
@@ -1,1 +1,14 @@
+const colors = require("colors");
 
+module.exports = async (client) => {
+  
+  console.log(`Xp bot connected!`.green);
+  
+  client.user.setPresence({
+    statut: "online",
+    activity: {
+      name: "Xp bot created by horayo",
+      type: "STREAMING",
+    }
+  })
+}
