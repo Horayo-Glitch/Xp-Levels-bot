4f31712e41c1eedb5b812c11d990be88c9858b21
Horayo <74557443+Horayo-Glitch@users.noreply.github.com>
2021-03-26 20:38:16 +0100

    Update leaderboard.js

--- Commands/leaderboard.js
+++ Commands/leaderboard.js
@@ -1,1 +1,14 @@
+const { MessageEmbed } = require("discord.js");
+const Levels = require("discord-xp");
+const canvacord = require("canvacord");
 
+module.exports.run = async (client, message, args) => {
+  
+  
+}
+
+module.exports.help = {
+  name: "leaderboard",
+  aliases: ["lb"],
+  description: "the levels leaderboard of the guild",
+}
