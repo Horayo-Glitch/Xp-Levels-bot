ba532bcf5689c119f3420e7f99de10c2bcaea7e2
Horayo <74557443+Horayo-Glitch@users.noreply.github.com>
2021-03-26 20:33:52 +0100

    Update rank.js

--- Commands/rank.js
+++ Commands/rank.js
@@ -1,1 +1,36 @@
+const { MessageEmbed } = require("discord.js");
+const Levels = require("discord-xp");
+const canvacord = require("canvacord");
 
+module.exports.run = async (client, message, args) => {
+  
+  const target = message.author;
+  
+  const user = Levels.fetch(target.id, message.guild.id);*
+  if (!user) return message.reply("You don't have any xp, try to send messages!");
+  
+  const neededXp = Levels.xpFor(parseInt(user.level) + 1);
+
+ const rank = new canvacord.Rank()
+    .setAvatar(target.displayAvatarURL({ dynamic: false, format: 'png' }))
+    .setCurrentXP(user.xp)
+    .setRequiredXP(neededXp)
+    .setLevel(user.level)
+    .setRank(user.rank)
+    .setStatus(target.presence.status)
+    .setProgressBar("#FFA500", "COLOR")
+    .setUsername(target.username)
+    .setDiscriminator(target.discriminator);
+rank.build()
+    .then(data => {
+        const attachment = new Discord.MessageAttachment(data, "rank.png");
+        message.channel.send(attachment);
+    });
+  
+}
+
+module.exports.help = {
+  name: "rank",
+  aliases: ["rang"],
+  description: "ur rank and level",
+}
