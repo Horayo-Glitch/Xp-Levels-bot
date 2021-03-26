3e61604680051eaa02be79d9ba6c978d199eef0e
Horayo <74557443+Horayo-Glitch@users.noreply.github.com>
2021-03-26 20:44:44 +0100

    Update leaderboard.js

--- Commands/leaderboard.js
+++ Commands/leaderboard.js
@@ -4,6 +4,22 @@ const canvacord = require("canvacord");
 
 module.exports.run = async (client, message, args) => {
   
+  const rawLeaderboard = await Levels.fetchLeaderboard(message.guild.id, 5);
+
+if (rawLeaderboard.length < 1) return reply("Nobody in the leaderboard.");
+
+const leaderboard = await Levels.computeLeaderboard(client, rawLeaderboard, true);
+
+const lb = leaderboard.map(e => `${e.position}. ${e.username}#${e.discriminator}\nLevel: ${e.level}\nXP: ${e.xp.toLocaleString()}`);
+
+   let embed = new MessageEmbed()
+   .setColor("RANDOM")
+   .setThumbnail(client.user.displayAvatarURL())
+   .setTitle("**__LEADERBOARD__**\n\n")
+   .setDescription(lb.join(" "))
+   .setFooter(`${client.user.tag} Bot | Leaderboard`, client.user.displayAvatarURL())
+   .setTimestamp()
+   message.channel.send(embed)
   
 }
 
