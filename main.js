7c7e0ef7d268d1222aaa61f5f929ecd7168810a1
Horayo <74557443+Horayo-Glitch@users.noreply.github.com>
2021-03-26 20:15:07 +0100

    Update main.js

--- main.js
+++ main.js
@@ -6,6 +6,8 @@ const Levels = require("discord-xp");
 const fs = require("fs");
 const client = new Client();
 
+Levels.setUrl("mongodb://..."); // a valid mongodb !
+
 client.commands = new Collection()
 
 const loadEvents = (dir = "./events") => {
@@ -20,7 +22,7 @@ const loadEvents = (dir = "./events") => {
 };
 loadEvents();
 
-const loadCommands = (dir = "./commands") => {
+const loadCommands = (dir = "./Commands") => {
 fs.readdirSync(dir).forEach(dirs => {
   const command = fs.readdirSync(`${dir}/${dirs}/`).filter(files => files.endsWith(".js"));
   
@@ -35,6 +37,19 @@ fs.readdirSync(dir).forEach(dirs => {
 });
 };
 loadCommands();
+
+client.on("message", async (message) => {
+  if (!message.guild) return;
+  if (message.author.bot) return;
+  
+  const randomAmountOfXp = Math.floor(Math.random() * 29) + 1; // Min 1, Max 30
+  const hasLeveledUp = await Levels.appendXp(message.author.id, message.guild.id, randomAmountOfXp);
+  if (hasLeveledUp) {
+    const user = await Levels.fetch(message.author.id, message.guild.id);
+    message.channel.send(`${message.author}, congratulations! You have leveled up to **${user.level}**. :tada:`);
+  }
+  
+});
 
 
 client.login(TOKEN);
