f6817845ec03c12de04bbb8ccb73a8cab48c48b8
Horayo <74557443+Horayo-Glitch@users.noreply.github.com>
2021-03-26 20:49:05 +0100

    Update message.js

--- events/client/message.js
+++ events/client/message.js
@@ -1,1 +1,17 @@
+const { PREFIX } = require("./../../config.js");
 
+module.exports = async (client, message) => {
+  
+  if (message.channel.type === "dm") return;
+  if (message.channel.type === "group") return;
+  if (!message.content.startsWith(PREFIX)) return;
+  if (message.author.bot) return;
+  
+  var args = message.content.slice(PREFIX.length).split('/+/');
+  let cmdName = args.shift().toLowerCase();
+  let cmd = client.commands.get(cmdName) || client.commands.get(client.aliases.get(cmdName));
+  if (!cmd) return;
+  
+  cmd.run(client, message, args)
+  
+}
